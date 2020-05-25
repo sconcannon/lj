ea4a85e9df1cfa6e4ee49d193504d79a274025e7
sconcannon <sean.concannon@gmail.com>
2020-05-25 10:15:26 -0700

    edits to ch2 main.js as a result of jsline

--- ch2/main.js
+++ ch2/main.js
@@ -1,17 +1,15 @@
 'use strict';
 
 function draw(n) {
-    if (n < 1 || n > 52) {
-        throw new Error('you must draw between 1 and 52 cards!');
-    };
-    const deck = [];
-    for(let suit of ['\u2660', '\u2663', '\u2665', '\u2666']) {
+    if (n < 1 || n > 52) {throw new Error('you must draw between 1 and 52 cards!'); }
+    var deck = [];
+    for (let suit of ['\u2660', '\u2663', '\u2665', '\u2666']) {
         for(let rank of 'A,2,3,4,5,6,7,8,9,10,J,Q,K'.split(',')) {
             deck.push(`${rank}${suit}`);
         }
     }
     const hand = [];
-    while(n) {
+    while (n) {
         hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
         console.log(deck.length);
         n--;
