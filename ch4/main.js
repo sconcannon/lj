fe6db3bab90b0ad9000349f669e75d80aad0e46e
sconcannon <sean.concannon@gmail.com>
2020-05-25 20:52:11 -0700

    started work in ch 4

---
+++ ch4/main.js
@@ -0,0 +1,14 @@
+let funds = 50;
+
+// returns a random integer in the range [m,n] (inclusive)
+function rand(m, n) {
+	return m + Math.floor((n - m + 1)*Math.random());
+}
+
+while (funds > 0 && funds < 100) {
+	// place bets
+
+	// roll dice
+
+	// collect winnings
+}
