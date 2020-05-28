78acfb399f993e51e23b4fea23b1aae5fe47e16a
sconcannon <sean.concannon@gmail.com>
2020-05-27 20:55:01 -0700

    further work on ch4

--- ch4/main.js
+++ ch4/main.js
@@ -1,14 +1,53 @@
-let funds = 50;
-
 // returns a random integer in the range [m,n] (inclusive)
 function rand(m, n) {
 	return m + Math.floor((n - m + 1)*Math.random());
 }
 
+// randomly returns a string representing one of the six
+// Crown and Anchor faces
+function randFace() {
+	return["crown","anchor","heart","spade", "club", "diamond"][rand(0,5)];
+}
+
+let funds = 50;    // starting condition
+let round = 0;
+
 while (funds > 0 && funds < 100) {
+	round++;
+	console.log(`Round ${round}:`);
+	console.log(`Starting funds: ${funds}p`);
 	// place bets
+	const bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0 };
+	let totalBet = rand(1, funds);
+	if(totalBet === 7) {
+		totalBet = funds;
+	} else {
+		// distribute total bet
+		let remaining = totalBet;
+		do {
+			let bet = rand(1,remaining);
+			let face = randFace();
+			bets[face] = bets[face] + bet;
+			remaining = remaining - bet;
+		} while (remaining > 0);
+	}
+	funds = funds - totalBet;
+	console.log('\tbets: ' + Object.keys(bets).map(face=> `${face}: ${bets[face]} pence`).join(', ') + ` (total: ${totalBet} pence)`);
 
 	// roll dice
+	const hand = [];
+	for(let roll=0; roll < 3; roll++) {
+		hand.push(randFace());
+	}
+	console.log(`\thand: ${hand.join(', ')}`);
 
 	// collect winnings
+	let winnings = 0;
+	for(let die=0; die<hand.length; die++ ) {
+		let face = hand[die];
+		if (bets[face] > 0) winnings = winnings + bets[face];
+	}
+	funds = funds + winnings;
+	console.log(`\twinnings: ${winnings}`);
 }
+console.log(`\tending funds: ${funds}`);
